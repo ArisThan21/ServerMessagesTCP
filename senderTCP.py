fbac1f589c282aba5e32f64e1c64099a55d601af
ArisThan21 <159693751+ArisThan21@users.noreply.github.com>
2024-04-13 13:26:55 +0300

    senderTCP.py

--- senderTCP.py
+++ senderTCP.py
@@ -1,16 +1,37 @@
+import tkinter as tk
 import socket
-import time
-HOST = 'Servers IP'
-PORT = "Server port"
 
-sent = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
 
+def TcpMessage():
+    try:
+        message = entry.get()
+        sent.send(message.encode('utf-8'))
+        text_widget.config(state=tk.NORMAL)
+        text_widget.delete(1.0, tk.END)
+        text_widget.insert(tk.END, sent.recv(1024).decode('utf-8'))
+        text_widget.config(state=tk.DISABLED)
+    except Exception as e:
+        text_widget.insert(tk.END, f"Κενο Text :{e}")
+
+
+root = tk.Tk()
+root.title("TCP MESSAGES!")
+
+HOST = 'Your Local IP'
+PORT = The port of the server
+
+sent = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 sent.connect((HOST, PORT))
 
-i = 1
-while i != 0:
-    message = input("Message to Server :")
-    sent.send(message.encode('utf-8'))
-    print(sent.recv(1024).decode('utf-8'))
-    i = int(input("What do you want to continue? (0=NO,1=YES)"))
+name_label = tk.Label(root, text="Message: ")
+name_label.pack()
+entry = tk.Entry(root)
+entry.pack()
 
+button = tk.Button(root, text="Εμφάνιση Μηνύματος", command=TcpMessage)
+button.pack()
+
+text_widget = tk.Text(root, height=10, width=50)
+text_widget.pack()
+
+root.mainloop()
