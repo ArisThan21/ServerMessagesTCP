3845f8152ace745aaab71f639dc19d61bbba97f7
ArisThan21 <159693751+ArisThan21@users.noreply.github.com>
2024-04-13 13:40:05 +0300

    server.py

--- server.py
+++ server.py
@@ -3,22 +3,26 @@ import socket
 server = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
 
 HOST = socket.gethostbyname(socket.gethostname())
-PORT = "Open port"
+PORT = 456
 server.bind((HOST, PORT))
-server1 =socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
-server1.bind((HOST, PORT+1))
 server.listen(5)
 message = ''
 
 while message != '0':
-    communication , address = server.accept()
-    print(f"Connection Compl..! {address} ")
-    while True:
-         message = communication.recv(1024).decode('utf-8')
-         print(f"We recived : {message}")
-         if message == '0':
-             communication.close()
-             print(f"The connection has close {address}")
-         else:
-             message = input("Message :")
-             communication.send(message.encode('utf-8'))
+    try:
+       communication , address = server.accept()
+       print(f"Connection Compl..! {address} ")
+       while True:
+          message = communication.recv(1024).decode('utf-8')
+          print(f"We recived : {message}")
+          if message == '0':
+              communication.close()
+              print(f"The connection has close {address}")
+          else:
+              message = input("Message :")
+              communication.send(message.encode('utf-8'))
+    except Exception as e:
+        print(e)
+
+
+server.close()
